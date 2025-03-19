410a91cdf50be20af7b2bf80e5798817e0ed6196
Amith Paul <amithpaulsiju@gmail.com>
2025-03-19 20:20:37 +0530

    changed it a bit to accept new dataset and also plots

--- LSTMdeftrain.py
+++ LSTMdeftrain.py
@@ -1,16 +1,25 @@
 import numpy as np
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import LSTM, Dense, Input
+from tensorflow.keras.layers import Masking, LSTM, Dense, Input
 from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras import backend as K
+import tensorflow as tf
+tf.config.set_visible_devices([], 'GPU')
+
+# Clear the Keras session to reset the model's state
+K.clear_session()
 
 early_stop = EarlyStopping(monitor="val_loss", patience=10, restore_best_weights=True)
 
 # Load preprocessed dataset
-data = np.load("scaled_dataset.npy").astype(np.float32)  # Ensure float32
+data = np.load("processed_datasets/scaled_minmax.npy").astype(np.float32)  # Ensure float32
+
+# Print the first 10 rows
+print("First 10 rows of the dataset:\n", data[:10])
 input_dim = data.shape[1]  # Number of features
 
 # Create sequences
-seq_length = 100  # Adjust based on your dataset
+seq_length = 180  # Adjust based on your dataset
 def create_sequences(data, seq_length):
     sequences = []
     for i in range(len(data) - seq_length):
@@ -24,22 +33,25 @@ X_train = create_sequences(data, seq_length)
 timesteps, features = X_train.shape[1], X_train.shape[2]
 
 input_layer = Input(shape=(timesteps, features))
-encoded = LSTM(64, activation="relu", return_sequences=True)(input_layer)
-encoded = LSTM(32, activation="relu", return_sequences=False)(encoded)
+
+# Apply masking to ignore -1 values
+masked_input = Masking(mask_value=-1)(input_layer)
+encoded = LSTM(64, activation="relu", return_sequences=True)(masked_input)
+encoded = LSTM(32, activation="relu", return_sequences=True)(encoded)
 
 decoded = Dense(64, activation="relu")(encoded)
 decoded = Dense(features, activation="sigmoid")(decoded)
 
 autoencoder = Model(input_layer, decoded)
-autoencoder.compile(optimizer="adam", loss="mse",jit_compile=True)
+autoencoder.compile(optimizer="adam", loss="mse")
 
 # Train the model
 autoencoder.fit(
-    X_train, X_train[:, -1, :], 
-    epochs=100, batch_size=64, 
+    X_train, X_train, 
+    epochs=200, batch_size=256, 
     validation_split=0.1, 
     callbacks=[early_stop]
 )
 
 # Save the model
-autoencoder.save("lstm_autoencoder.keras")
+autoencoder.save("lstm_autoencoderV2.keras")
